d6b01085b8bc2a25268595cb005e69f57a0b82e2
Wojciech Wichary <65503273+brylocode@users.noreply.github.com>
2023-02-10 12:10:30 +0100

    fix

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-(function () {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+// (function () {
+//     const second = 1000,
+//         minute = second * 60,
+//         hour = minute * 60,
+//         day = hour * 24;
 
-    const countDown = new Date("April 4, 2023 16:00:00").getTime(),
-        x = setInterval(function () {
+//     const countDown = new Date("April 4, 2023 16:00:00").getTime(),
+//         x = setInterval(function () {
 
-            const now = new Date("March 4, 2023 16:00:00").getTime(),
-                distance = countDown - now;
+//             const now = new Date("March 4, 2023 16:00:00").getTime(),
+//                 distance = countDown - now;
 
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+//             document.getElementById("days").innerText = Math.floor(distance / (day)),
+//                 document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+//                 document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+//                 document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-            //do something later when date is reached
-            if (distance < 0) {
-                document.getElementById("headline").innerText = "It's my birthday!";
-                document.getElementById("countdown").style.display = "none";
-                document.getElementById("content").style.display = "block";
-                clearInterval(x);
-            }
-            //seconds
-        }, 0)
-}());
+//             //do something later when date is reached
+//             if (distance < 0) {
+//                 document.getElementById("headline").innerText = "It's my birthday!";
+//                 document.getElementById("countdown").style.display = "none";
+//                 document.getElementById("content").style.display = "block";
+//                 clearInterval(x);
+//             }
+//             //seconds
+//         }, 0)
+// }());
 
 
 //menu 
